5aa18cb18a8ab31b9ee04227b64b43a3aad5c711
Torendra Rasik <torendrarasik@gmail.com>
2024-09-03 21:09:55 -0400

    (Frontend): Adds Theme Controller
    - Adds themes that switch primary and secondary colors
    - Uses Mode watcher to switch themes

--- frontend/src/lib/components/navbar/Settings.svelte
+++ frontend/src/lib/components/navbar/Settings.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { setMode, resetMode } from 'mode-watcher';
+	import { setMode, resetMode, setTheme, theme } from 'mode-watcher';
 
 	import IconSettings from '~icons/ic/round-settings';
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
@@ -21,6 +21,17 @@
 				<DropdownMenu.Item on:click={() => setMode('light')}>Light</DropdownMenu.Item>
 				<DropdownMenu.Item on:click={() => setMode('dark')}>Dark</DropdownMenu.Item>
 				<DropdownMenu.Item on:click={() => resetMode()}>System</DropdownMenu.Item>
+			</DropdownMenu.SubContent>
+		</DropdownMenu.Sub>
+		<DropdownMenu.Sub>
+			<DropdownMenu.SubTrigger>Themes</DropdownMenu.SubTrigger>
+			<DropdownMenu.SubContent>
+				<DropdownMenu.Item on:click={() => setTheme('winter')}>Winter</DropdownMenu.Item>
+				<DropdownMenu.Item on:click={() => setTheme('cmyk')}>CMYK</DropdownMenu.Item>
+				<DropdownMenu.Item on:click={() => setTheme('cupcake')}>Cupcake</DropdownMenu.Item>
+				<DropdownMenu.Item on:click={() => setTheme('night')}>Night</DropdownMenu.Item>
+				<DropdownMenu.Item on:click={() => setTheme('dracula')}>Dracula</DropdownMenu.Item>
+				<DropdownMenu.Item on:click={() => setTheme('sunset')}>Sunset</DropdownMenu.Item>
 			</DropdownMenu.SubContent>
 		</DropdownMenu.Sub>
 		<DropdownMenu.Sub>
