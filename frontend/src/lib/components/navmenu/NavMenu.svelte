274d59e0a123352de512da0b2bb40cd9d1f2aff0
Torendra Rasik <torendrarasik@gmail.com>
2024-09-11 13:30:24 -0400

    (Frontend): Adds Home Menu Component
    - Adds navigation menu to home
    - Lays out foundation for it to be used in projects page.

---
+++ frontend/src/lib/components/navmenu/NavMenu.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import * as Card from '$lib/components/ui/card';
+	import * as Carousel from '$lib/components/ui/carousel';
+
+	export let basis: string;
+	export let maxWidth: string;
+	export let menuIcons;
+</script>
+
+<Carousel.Root class="w-full px-[clamp(1rem,4vw,6rem)] {maxWidth}">
+	<Carousel.Content class="-ml-1 pb-8">
+		{#each menuIcons as item}
+			<Carousel.Item class="{basis} pl-1 transition-transform duration-300 hover:translate-y-8">
+				<div class="group relative p-1">
+					<Card.Root>
+						<Card.Content class="flex aspect-square items-center justify-center p-6">
+							<svelte:component this={item.icon} class="text-6xl text-primary sm:text-8xl " />
+						</Card.Content>
+					</Card.Root>
+					<span
+						class="absolute -top-8 left-0 right-0 mx-auto w-fit text-2xl opacity-0 transition-opacity duration-300 group-hover:opacity-100"
+					>
+						{item.header}
+					</span>
+				</div>
+			</Carousel.Item>
+		{/each}
+	</Carousel.Content>
+</Carousel.Root>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,1 +1,28 @@
-<h1 class="px-[clamp(0.5rem,3vw,3.5rem)] text-6xl">Welcome to SvelteKit</h1>
+<script>
+	import IconAbout from '~icons/hugeicons/alien-01';
+	import IconWork from '~icons/hugeicons/work-history';
+	import IconBlog from '~icons/hugeicons/pencil-edit-01';
+	import IconContact from '~icons/hugeicons/contact-01';
+	import NavMenu from '$lib/components/navmenu/NavMenu.svelte';
+
+	const menuIcons = [
+		{
+			icon: IconAbout,
+			header: 'About Me'
+		},
+		{
+			icon: IconWork,
+			header: 'Experience'
+		},
+		{
+			icon: IconBlog,
+			header: 'Blog'
+		},
+		{
+			icon: IconContact,
+			header: 'Contact Me'
+		}
+	];
+</script>
+
+<NavMenu basis="basis-1/3 sm:basis-1/4" maxWidth="max-w-5xl" {menuIcons} />
